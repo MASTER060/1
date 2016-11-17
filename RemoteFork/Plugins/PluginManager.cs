262b9db887fcdf5d862eaf12ed495e6ac89ff501
abardashevskiy <andrey.bardashevskiy@omnigon.com>
2016-11-17 21:41:04 +0200

    improved plugin manager to load assemblies from libraries not only from scripts

--- RemoteFork/Plugins/PluginManager.cs
+++ RemoteFork/Plugins/PluginManager.cs
@@ -8,20 +8,25 @@ using System.Security.Cryptography;
 using Microsoft.CSharp;
 using RemoteFork.Properties;
 
-namespace RemoteFork.Plugins {
-    internal class PluginManager {
+namespace RemoteFork.Plugins
+{
+    internal class PluginManager
+    {
         public static readonly PluginManager Instance = new PluginManager();
 
         private readonly Dictionary<string, PluginInstance> plugins = new Dictionary<string, PluginInstance>();
 
-        private PluginManager() {
+        private PluginManager()
+        {
             LoadPlugins();
         }
 
-        public static Assembly CompileAssembly(string sourceFile) {
+        public static Assembly CompileAssembly(string sourceFile)
+        {
             var codeProvider = new CSharpCodeProvider();
 
-            var compilerParameters = new CompilerParameters {
+            var compilerParameters = new CompilerParameters
+            {
                 GenerateExecutable = false,
                 GenerateInMemory = true,
                 IncludeDebugInformation = true
@@ -38,63 +43,112 @@ namespace RemoteFork.Plugins {
             return assembly;
         }
 
-        private void LoadPlugins() {
+        private void LoadPlugins()
+        {
             string pathPlugins = Path.Combine(Environment.CurrentDirectory, "Plugins");
-            if (Directory.Exists(pathPlugins)) {
-                var files = Directory.GetFiles(pathPlugins, "*.cs");
-                foreach (string file in files) {
-                    try {
-                        var results = CompileAssembly(file);
-                        var types = results.GetExportedTypes();
-                        if (types.Length > 0) {
-                            var type = types[0];
-                            if (type.BaseType == typeof (BasePlugin)) {
-                                var attributes = type.GetCustomAttributes(true);
+            if (Directory.Exists(pathPlugins))
+            {
+                LoadScripts(pathPlugins);
+                LoadAssemblies(pathPlugins);
+            }
+        }
 
-                                if (attributes.Length > 0) {
-                                    var attribute =
-                                        (PluginAttribute)
-                                            attributes.FirstOrDefault(i => i.GetType() == typeof (PluginAttribute));
-                                    if (attribute != null) {
-                                        var plugin = new PluginInstance(GetChecksum(file), results, type, attribute);
-                                        if (!plugins.ContainsKey(plugin.Name)) {
-                                            plugins.Add(plugin.Id, plugin);
-                                        }
-                                    }
-                                }
+        private void LoadAssemblies(string pathPlugins)
+        {
+            DirectoryInfo dir = new DirectoryInfo(pathPlugins);
+
+            foreach (FileInfo file in dir.GetFiles("*.dll"))
+            {
+                try
+                {
+                    LoadAssembly(Assembly.LoadFrom(file.FullName), GetChecksum(file.FullName));
+                }
+                catch (Exception e)
+                {
+                    Logger.Debug("LoadPlugins->{0}: {1}", file, e);
+                }
+            }
+        }
+
+        private void LoadScripts(string pathPlugins)
+        {
+            var files = Directory.GetFiles(pathPlugins, "*.cs");
+            foreach (string file in files)
+            {
+                try
+                {
+                    LoadAssembly(CompileAssembly(file), GetChecksum(file));
+                }
+                catch (Exception ex)
+                {
+                    Logger.Debug("LoadPlugins->{0}: {1}", file, ex);
+                }
+            }
+        }
+
+        private void LoadAssembly(Assembly assembly, string hash)
+        {
+            foreach (Type type in assembly.GetExportedTypes())
+            {
+                if (type.IsSubclassOf(typeof(BasePlugin)) && type.IsAbstract == false)
+                {
+                    var attributes = type.GetCustomAttributes(true);
+
+                    if (attributes.Length > 0)
+                    {
+                        var attribute =
+                            (PluginAttribute) attributes.FirstOrDefault(i => i.GetType() == typeof(PluginAttribute));
+                        if (attribute != null)
+                        {
+                            var plugin = new PluginInstance(hash, assembly, type, attribute);
+                            if (!plugins.ContainsKey(plugin.Id))
+                            {
+                                plugins.Add(plugin.Id, plugin);
+
+                                Logger.Debug("Loaded plugin [id: {0}, name: {1}, type: {2}, version: {3}]", plugin.Id, plugin.Name, type.FullName, plugin.Version);
                             }
                         }
-                    } catch (Exception ex) {
-                        Logger.Debug("LoadPlugins->{0}: {1}", file, ex);
                     }
                 }
             }
         }
-        private static string GetChecksum(string file) {
-            using (var stream = File.OpenRead(file)) {
+
+        private static string GetChecksum(string file)
+        {
+            using (var stream = File.OpenRead(file))
+            {
                 var sha = new SHA256Managed();
                 byte[] checksum = sha.ComputeHash(stream);
                 return BitConverter.ToString(checksum).Replace("-", string.Empty);
             }
         }
 
-        public void ReimportPlugins() {
+        public void ReimportPlugins()
+        {
             plugins.Clear();
             LoadPlugins();
         }
 
-        public void RemovePlugin(string name) {
-            if (plugins.ContainsKey(name)) {
+        public void RemovePlugin(string name)
+        {
+            if (plugins.ContainsKey(name))
+            {
                 plugins.Remove(name);
             }
         }
 
-        public Dictionary<string, PluginInstance> GetPlugins(bool filtering = true) {
-            if (filtering) {
+        public Dictionary<string, PluginInstance> GetPlugins(bool filtering = true)
+        {
+            if (filtering)
+            {
                 var dict = new Dictionary<string, PluginInstance>();
 
-                if (Settings.Default.Plugins && Settings.Default.EnablePlugins != null) {
-                    foreach (var plugin in plugins.Where(plugin => Settings.Default.EnablePlugins.Contains(plugin.Value.Key))) {
+                if (Settings.Default.Plugins && Settings.Default.EnablePlugins != null)
+                {
+                    foreach (
+                        var plugin in plugins.Where(plugin => Settings.Default.EnablePlugins.Contains(plugin.Value.Key))
+                    )
+                    {
                         dict.Add(plugin.Key, plugin.Value);
                     }
                 }
@@ -104,12 +158,20 @@ namespace RemoteFork.Plugins {
             return plugins;
         }
 
-        public PluginInstance GetPlugin(string name) {
-            if (plugins.ContainsKey(name)) {
-                if (Settings.Default.Plugins && Settings.Default.EnablePlugins != null) {
-                    if (Settings.Default.EnablePlugins.Contains(plugins[name].Key)) {
-                        return plugins[name];
-                    }
+        public PluginInstance GetPlugin(string id)
+        {
+            if (plugins.ContainsKey(id))
+            {
+                if (Settings.Default.Plugins && Settings.Default.EnablePlugins != null)
+                {
+                    #if DEBUG
+                        return plugins[id];
+                    #else
+                        if (Settings.Default.EnablePlugins.Contains(plugins[id].Key))
+                        {
+                            return plugins[id];
+                        }
+                    #endif
                 }
             }
 
