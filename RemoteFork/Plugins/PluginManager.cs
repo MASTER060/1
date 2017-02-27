14885c895a4f213bea64b02d15a278c4d4733367
alexkdpu <alexkdpu@mail.ru>
2017-02-27 13:02:46 +0200

    Add files via upload

--- RemoteFork/Plugins/PluginManager.cs
+++ RemoteFork/Plugins/PluginManager.cs
@@ -9,81 +9,118 @@ using Common.Logging;
 using Microsoft.CSharp;
 using RemoteFork.Properties;
 
-namespace RemoteFork.Plugins {
-    internal class PluginManager {
+namespace RemoteFork.Plugins
+{
+    internal class PluginManager
+    {
         private static readonly ILog Log = LogManager.GetLogger(typeof(PluginManager));
 
         public static readonly PluginManager Instance = new PluginManager();
 
         private readonly Dictionary<string, PluginInstance> plugins = new Dictionary<string, PluginInstance>();
 
-        private PluginManager() {
+        private PluginManager()
+        {
             LoadPlugins();
         }
 
-        public static Assembly CompileAssembly(string sourceFile) {
+        public static Assembly CompileAssembly(string sourceFile)
+        {
             var codeProvider = new CSharpCodeProvider();
 
-            var compilerParameters = new CompilerParameters {
+            var compilerParameters = new CompilerParameters
+            {
                 GenerateExecutable = false,
                 GenerateInMemory = true,
                 IncludeDebugInformation = true
             };
 
-            compilerParameters.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+            compilerParameters.ReferencedAssemblies.Add(Assembly.GetAssembly(typeof(IPlugin)).Location);
             compilerParameters.ReferencedAssemblies.Add("System.dll");
             compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
 
             var result = codeProvider.CompileAssemblyFromFile(compilerParameters, sourceFile);
 
-            var assembly = result.CompiledAssembly;
+            var hasCompileErrors = false;
+            foreach (CompilerError ce in result.Errors)
+            {
+                Log.Debug(m => m("{0}({1},{2}): {3} {4}: {5}", ce.FileName, ce.Line, ce.Column, ce.IsWarning ? "warning" : "error", ce.ErrorNumber, ce.ErrorText));
 
-            return assembly;
+                if (!ce.IsWarning)
+                {
+                    hasCompileErrors = true;
+                }
+            }
+
+            if (hasCompileErrors)
+            {
+                throw new ApplicationException("Compile errors occured, see debug log for more details.");
+            }
+
+            return result.CompiledAssembly;
         }
 
-        private void LoadPlugins() {
+        private void LoadPlugins()
+        {
             var pathPlugins = Path.Combine(Environment.CurrentDirectory, "Plugins");
 
-            if (Directory.Exists(pathPlugins)) {
+            if (Directory.Exists(pathPlugins))
+            {
                 LoadScripts(pathPlugins);
                 LoadAssemblies(pathPlugins);
             }
         }
 
-        private void LoadAssemblies(string pathPlugins) {
+        private void LoadAssemblies(string pathPlugins)
+        {
             var dir = new DirectoryInfo(pathPlugins);
 
-            foreach (var file in dir.GetFiles("*.dll")) {
-                try {
+            foreach (var file in dir.GetFiles("*.dll"))
+            {
+                try
+                {
                     LoadAssembly(Assembly.LoadFrom(file.FullName), GetChecksum(file.FullName));
-                } catch (Exception e) {
-                    Log.Debug(m => m("LoadPlugins->{0}: {1}", file, e));
+                }
+                catch (Exception e)
+                {
+                    Log.Error(m => m("LoadPlugins->{0}: {1}", file, e));
                 }
             }
         }
 
-        private void LoadScripts(string pathPlugins) {
+        private void LoadScripts(string pathPlugins)
+        {
             var files = Directory.GetFiles(pathPlugins, "*.cs");
 
-            foreach (var file in files) {
-                try {
+            foreach (var file in files)
+            {
+                try
+                {
                     LoadAssembly(CompileAssembly(file), GetChecksum(file));
-                } catch (Exception ex) {
-                    Log.Debug(m => m("LoadPlugins->{0}: {1}", file, ex));
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(m => m("LoadPlugins->{0}: {1}", file, ex));
                 }
             }
         }
 
-        private void LoadAssembly(Assembly assembly, string hash) {
-            foreach (var type in assembly.GetExportedTypes()) {
-                if (typeof(IPlugin).IsAssignableFrom(type) && (type.IsAbstract == false)) {
+        private void LoadAssembly(Assembly assembly, string hash)
+        {
+            foreach (var type in assembly.GetExportedTypes())
+            {
+                if (typeof(IPlugin).IsAssignableFrom(type) && (type.IsAbstract == false))
+                {
                     var attributes = type.GetCustomAttributes(true);
 
-                    if (attributes.Length > 0) {
-                        var attribute = (PluginAttribute) attributes.FirstOrDefault(i => i.GetType() == typeof(PluginAttribute));
-                        if (attribute != null) {
+                    if (attributes.Length > 0)
+                    {
+                        var attribute = (PluginAttribute)attributes.FirstOrDefault(i => i.GetType() == typeof(PluginAttribute));
+                        if (attribute != null)
+                        {
                             var plugin = new PluginInstance(hash, assembly, type, attribute);
-                            if (!plugins.ContainsKey(plugin.Id)) {
+                            if (!plugins.ContainsKey(plugin.Id))
+                            {
                                 plugins.Add(plugin.Id, plugin);
 
                                 Log.Debug(m => m(
@@ -101,31 +138,39 @@ namespace RemoteFork.Plugins {
             }
         }
 
-        private static string GetChecksum(string file) {
-            using (var stream = File.OpenRead(file)) {
+        private static string GetChecksum(string file)
+        {
+            using (var stream = File.OpenRead(file))
+            {
                 var sha = new SHA256Managed();
                 var checksum = sha.ComputeHash(stream);
                 return BitConverter.ToString(checksum).Replace("-", string.Empty);
             }
         }
 
-        public void ReimportPlugins() {
+        public void ReimportPlugins()
+        {
             plugins.Clear();
             LoadPlugins();
         }
 
-        public void RemovePlugin(string name) {
+        public void RemovePlugin(string name)
+        {
             if (plugins.ContainsKey(name))
                 plugins.Remove(name);
         }
 
-        public Dictionary<string, PluginInstance> GetPlugins(bool filtering = true) {
-            if (filtering) {
+        public Dictionary<string, PluginInstance> GetPlugins(bool filtering = true)
+        {
+            if (filtering)
+            {
                 var dict = new Dictionary<string, PluginInstance>();
 
-                if (Settings.Default.Plugins && (Settings.Default.EnablePlugins != null)) {
+                if (Settings.Default.Plugins && (Settings.Default.EnablePlugins != null))
+                {
 #if DEBUG
-                    foreach (var plugin in plugins) {
+                    foreach (var plugin in plugins)
+                    {
                         dict.Add(plugin.Key, plugin.Value);
                     }
 #else
@@ -140,9 +185,12 @@ namespace RemoteFork.Plugins {
             return plugins;
         }
 
-        public PluginInstance GetPlugin(string id) {
-            if (plugins.ContainsKey(id)) {
-                if (Settings.Default.Plugins && (Settings.Default.EnablePlugins != null)) {
+        public PluginInstance GetPlugin(string id)
+        {
+            if (plugins.ContainsKey(id))
+            {
+                if (Settings.Default.Plugins && (Settings.Default.EnablePlugins != null))
+                {
 #if DEBUG
                     return plugins[id];
 #else
